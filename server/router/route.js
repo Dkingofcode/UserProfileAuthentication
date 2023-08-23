d4556ec2de8eade60eaaa3b77704b340e176961c
David <doladepo128@gmail.com>
2023-08-23 15:56:52 +0100

    Created the backend server

---
+++ server/router/route.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+const router = Router();
+
+
+/** POST request */
+router.route('/register').post((req, res) => res.json('register route'));
+
+/** GET request */
+
+
+
+/** Patch request */
+
+
+/**  Delete request  */
+
+
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
